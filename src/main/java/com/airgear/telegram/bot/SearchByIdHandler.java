6c4de31bff83bc65073183acd02ed3b20c3baa4f
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2024-07-24 22:25:13 +0300

    photo output when searching by id

--- src/main/java/com/airgear/telegram/bot/SearchByIdHandler.java
+++ src/main/java/com/airgear/telegram/bot/SearchByIdHandler.java
@@ -1,14 +1,29 @@
 package com.airgear.telegram.bot;
 
 import com.airgear.telegram.dto.GoodsResponse;
+import com.airgear.telegram.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 
 @Component
 public class SearchByIdHandler implements MessageHandler {
+
+    private final ImageService imageService;
+
+    @Autowired
+    public SearchByIdHandler(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @Override
     public void handle(Update update, TelegramBot bot) {
@@ -25,6 +40,18 @@ public class SearchByIdHandler implements MessageHandler {
                 Long goodsId = Long.parseLong(messageText);
                 GoodsResponse goodsResponseDTO = bot.getGoodsService().getGoodsById(goodsId);
                 bot.sendResponse(chatId, goodsResponseDTO.toFormattedString());
+
+                try {
+                    Optional<byte[]> imageBytes = imageService.getFirstImageBytesByGoodsId(goodsId);
+                    if (imageBytes.isPresent()) {
+                        sendPhoto(chatId, bot, imageBytes.get());
+                    }
+                } catch (IOException e) {
+                    bot.sendResponse(chatId, "Не вдалося завантажити зображення.");
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
                 sendBackButton(chatId, bot);
             } catch (NumberFormatException e) {
                 bot.sendResponse(chatId, "Будь ласка, введіть валідний ID оголошення.");
@@ -51,5 +78,14 @@ public class SearchByIdHandler implements MessageHandler {
 
     private void sendBackButton(long chatId, TelegramBot bot) {
         bot.sendReplyKeyboard(chatId, "Натисніть кнопку, щоб повернутися назад:", Collections.singletonList("Назад"));
+    }
+
+    private void sendPhoto(long chatId, TelegramBot bot, byte[] imageBytes) throws TelegramApiException {
+        InputFile inputFile = new InputFile(new ByteArrayInputStream(imageBytes), "image.jpg");
+        SendPhoto sendPhoto = SendPhoto.builder()
+                .chatId(String.valueOf(chatId))
+                .photo(inputFile)
+                .build();
+        bot.execute(sendPhoto);
     }
 }
